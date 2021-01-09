da041d59d43359f64db0f939450647e9e3dd8135
João Sallas <joaovictorsallas@gmail.com>
2021-01-09 16:50:13 -0300

    PUT /orders/{id}/delivered

--- backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
@@ -30,4 +30,10 @@ public class OrderController {
                 .buildAndExpand(orderDTO.getId()).toUri();
         return ResponseEntity.created(uri).body(orderDTO);
     }
+
+    @PutMapping("/{id}/delivered")
+    public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id){
+        OrderDTO dto = orderService.setDelivered(id);
+        return ResponseEntity.ok().body(dto);
+    }
 }
